bddb23e59561023953599ed283b1f0f9ef757b00
Thaiph1308 <ryuamakusa123@gmail.com>
2018-11-12 21:11:25 +0700

    Update Predict CNN

--- PredictCNN.py
+++ PredictCNN.py
@@ -11,8 +11,23 @@ from keras.layers import Flatten
 from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import MaxPooling2D
 
+img_width = 28
+img_height = 28
+
 model=load_model("CNN.h5")
 model.summary()
+for layer in model.layers:
+    print(layer.get_input_at(0).get_shape().as_list())
+im=cv2.imread("digit.jpg")
+im=cv2.resize(im,(img_height,img_width))
+im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+print(np.shape(im))
+arr = np.reshape(im,(img_height,img_width))
+print(np.shape(arr))
+arr = np.expand_dims(arr,axis=0)
+arr = np.expand_dims(arr,axis=0)
+print(np.shape(arr))
+print(model.predict(arr))
 # (X_train, y_train), (X_test, y_test) = mnist.load_data()
 # print(np.shape(X_train))
 # print(model.predict(X_test[0]))
